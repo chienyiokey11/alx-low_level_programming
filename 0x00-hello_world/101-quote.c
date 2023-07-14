fdbdb1fdd4703f84b9ee4c5cc630550ff388a3c3
chienyiokey11 <chienyiokey@gmail.com>
2023-07-14 06:29:09 -0700

    hello c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - printing with another print function that is not printf or puts
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
+}
