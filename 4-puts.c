f815be4a04deae7855e8d666edb905d2090737f1
chienyiokey11 <chienyiokey@gmail.com>
2023-07-14 03:26:34 -0700

    hello c

---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print a simple message and exit
+ *
+ * Return: 0 - exit status zero if all ok
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+okey just started coding
