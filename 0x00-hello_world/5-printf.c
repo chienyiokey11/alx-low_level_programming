ce9fccdf59af43a0bdbc48c1499312228f75e7ce
chienyiokey11 <chienyiokey@gmail.com>
2023-07-14 03:46:50 -0700

    hello c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - using printf to print statment
+ *
+ * Return: (0) as nothing
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
