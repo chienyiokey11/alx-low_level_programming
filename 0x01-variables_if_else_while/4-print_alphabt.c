97ed904ebee0a34ce5edc8bb5038509c95b33d09
chienyiokey11 <chienyiokey@gmail.com>
2023-10-04 15:20:29 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - using putchar to print alphabet with exceptions
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int alpha = 'a';
+
+	while(alpha <= 'z')
+	{
+		if (alpha != 'e' && alpha != 'q')
+			putchar(alpha);
+		alpha++;
+	}
+		putchar('\n');
+	return (0);
+}	
+
